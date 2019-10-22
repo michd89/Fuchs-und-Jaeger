6aeec786acb927c6087c42393aa61afdf2619271
Michael Dietrich <michael.dietrich@s2010.tu-chemnitz.de>
2019-10-22 22:20:55 +0200

    Add coordinate labels onto chess board

--- game_pygame.py
+++ game_pygame.py
@@ -15,6 +15,10 @@ def main():
     WHITE = (255, 255, 255)
     game = FuchsUndJaeger()
     game.board.print_board()
+    testbild = pygame.image.load('chess.png').convert_alpha()
+    testbild = pygame.transform.scale(testbild, (int(768/2), int(1536/2)))
+    font = pygame.font.SysFont('Courier New', 20, False, False)
+
     while not done:
         # Event handling
         for event in pygame.event.get():
@@ -35,12 +39,29 @@ def main():
             for y in range(len(game.board.squares[x])):
                 color = BLACK if game.board.squares[x][y] == 'b' else WHITE
                 pygame.draw.rect(screen, color, (103+50*x, 103+50*y, 50, 50), 0)
+        # Coordinate labels
+        for i, letter in enumerate(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']):
+            text = font.render(letter, True, WHITE)
+            screen.blit(text, (122+i*50, 520))
+            text = pygame.transform.rotate(text, 180)
+            screen.blit(text, (122+i*50, 60))
+        for i, letter in enumerate(['8', '7', '6', '5', '4', '3', '2', '1', ]):
+            text = font.render(letter, True, WHITE)
+            screen.blit(text, (70, 117+i*50))
+            text = pygame.transform.rotate(text, 180)
+            screen.blit(text, (520, 117+i*50))
+
+
+        # Test
+        # screen.blit(testbild, [100, 100])
 
         # Update display
         pygame.display.flip()
 
         # Limit frame rate
         clock.tick(60)
+
+        # game.board.print_board()
 
     pygame.quit()
 
