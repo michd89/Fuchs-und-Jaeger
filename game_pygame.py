818e09bdc30594d2f19d389dfdcaf7957484b786
Michael Dietrich <michael.dietrich@s2010.tu-chemnitz.de>
2020-12-04 20:07:49 +0100

    clarify "magic numbers"

--- game_pygame.py
+++ game_pygame.py
@@ -5,17 +5,39 @@ from fuchsundjaeger import FuchsUndJaeger
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+peice_size = 64
+tile_size = 50
+
+border_start_x = 100
+border_start_y = 100
+border_width = 406
+border_height = 406
+border_thickness = 6  # May cause display errors if odd
+
+tiles_start_x = int(border_start_x + border_thickness / 2)
+tiles_start_y = int(border_start_y + border_thickness / 2)
+
+letter_width = 12  # Estimated, to put every letter roughly at the middle
+# letter_height = ???
+text_distance = 10  # Distance of text from board
+
 
 class PieceType(object):
     def __init__(self, piece, start_x, start_y):
-        self.rect = pygame.rect.Rect(start_x, start_y, 64, 64)
+        self.rect = pygame.rect.Rect(start_x, start_y, peice_size, peice_size)
         self.piece = piece
 
 
 class PieceSet:
     def __init__(self):
         self.image = pygame.image.load('chess.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (int(768 / 2), int(1536 / 2)))
+        image_width = 768
+        image_height = 1536
+        imace_scale = 0.5
+        self.image = pygame.transform.scale(self.image,
+                                            (int(image_width * imace_scale),
+                                             int(image_height * imace_scale))
+                                            )
         self.piece_tile_list = list()
 
     def add_piece_tile(self, piece, start_x, start_y):
@@ -31,37 +53,50 @@ class PieceSet:
 
 def init_pieceset(game):
     pieces = PieceSet()
-    pieces.add_piece_tile(game.fuchs, 64, 128)
-    pieces.add_piece_tile(game.jaeger[0], 0, 64)
-    pieces.add_piece_tile(game.jaeger[1], 0, 64)
-    pieces.add_piece_tile(game.jaeger[2], 0, 64)
-    pieces.add_piece_tile(game.jaeger[3], 0, 64)
+    pieces.add_piece_tile(game.fuchs, peice_size, peice_size * 2)  # In chess.png the knight is to the right of the pawn
+    pieces.add_piece_tile(game.jaeger[0], 0, peice_size)
+    pieces.add_piece_tile(game.jaeger[1], 0, peice_size)
+    pieces.add_piece_tile(game.jaeger[2], 0, peice_size)
+    pieces.add_piece_tile(game.jaeger[3], 0, peice_size)
     return pieces
 
 
 def draw_chessboard(screen, game, font):
     # Board border
-    pygame.draw.rect(screen, (102, 153, 153), (100, 100, 406, 406), 5)
+    border_color = (102, 153, 153)
+    pygame.draw.rect(screen, border_color,
+                     (border_start_x, border_start_y, border_width, border_height), border_thickness)
     # Chess fields
     for x in range(len(game.board.squares)):
         for y in range(len(game.board.squares[x])):
             color = WHITE if (x + y) % 2 == 0 else BLACK
-            pygame.draw.rect(screen, color, (103 + 50 * x, 103 + 50 * y, 50, 50), 0)
+            pygame.draw.rect(screen, color, (tiles_start_x + tile_size * x, tiles_start_y + tile_size * y,
+                                             tile_size, tile_size), 0)
     # Coordinate labels
+    letters_x = tiles_start_x + int(tile_size / 2) - int(letter_width / 2)
+    # Maybe it looks more even when rather the text height considered
+    letters_y_upper = tiles_start_y - border_thickness * 2 - letter_width - text_distance
+    letters_y_lower = tiles_start_y + tile_size * 8 + border_thickness + text_distance
     for i, letter in enumerate(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']):
         text = font.render(letter, True, WHITE)
-        screen.blit(text, (122 + i * 50, 520))
+        screen.blit(text, (letters_x + i * tile_size, letters_y_lower))
         text = pygame.transform.rotate(text, 180)
-        screen.blit(text, (122 + i * 50, 60))
-    for i, letter in enumerate(['8', '7', '6', '5', '4', '3', '2', '1', ]):
+        screen.blit(text, (letters_x + i * tile_size, letters_y_upper))
+
+    numbers_y = tiles_start_y + int(tile_size / 2) - letter_width
+    numbers_x_left = tiles_start_x - border_thickness - letter_width - text_distance
+    numbers_x_right = tiles_start_x + tile_size * 8 + border_thickness + text_distance
+    for i, letter in enumerate(['8', '7', '6', '5', '4', '3', '2', '1']):
         text = font.render(letter, True, WHITE)
-        screen.blit(text, (70, 117 + i * 50))
+        screen.blit(text, (numbers_x_left, numbers_y + i * tile_size))
         text = pygame.transform.rotate(text, 180)
-        screen.blit(text, (520, 117 + i * 50))
+        screen.blit(text, (numbers_x_right, numbers_y + i * tile_size))
 
 
 def get_tile_start(pos_x, pos_y):
-    return 96 + 50 * pos_x, 96 + 50 * pos_y
+    offset_x = tiles_start_x - border_thickness - 1  # Put more in the middle with minus one
+    offset_y = tiles_start_y - border_thickness - 1  # Put more in the middle with minus one
+    return offset_x + tile_size * pos_x, offset_y + tile_size * pos_y
 
 
 def draw_pieces(screen, pieceset):
@@ -99,7 +134,8 @@ def main():
     game = FuchsUndJaeger()
     game.new_game(2)
     square_rects = [
-        [pygame.Rect((103 + 50 * x, 103 + 50 * y), (50, 50)) for y in range(8)] for x in range(8)]
+        [pygame.Rect((tiles_start_x + tile_size * x, tiles_start_y + tile_size * y), (tile_size, tile_size))
+         for y in range(8)] for x in range(8)]
     selected_square = None
     target_square = None
     pieceset = init_pieceset(game)
@@ -141,8 +177,13 @@ def main():
         draw_chessboard(screen, game, font)
         draw_pieces(screen, pieceset)
         if selected_square:
-            pygame.draw.rect(screen, (0, 200, 0), (
-                103 + 50 * selected_square[0], 103 + 50 * selected_square[1], 50, 50), 4)
+            pygame.draw.rect(screen,
+                             (0, 200, 0),
+                             (tiles_start_x + tile_size * selected_square[0],
+                              tiles_start_y + tile_size * selected_square[1],
+                              tile_size,
+                              tile_size),
+                             border_thickness)
         if game.state == 'F':
             state = 'Turn Fuchs'
         elif game.state == 'J':
